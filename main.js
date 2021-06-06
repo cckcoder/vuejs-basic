6f48ac1c8ee03a7175b93b1813d001985ee05b2f
codewizz <c.chaiwat.ch@gmail.com>
2021-06-06 15:19:56 +0700

    add: rendering

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const app = Vue.createApp({
+    mounted() {
+        setInterval(() => {
+            this.counter++
+        }, 1000)
+    },
     data () {
         return {
-            product: 'Espresso'
+            counter: 0
         }
     }
 })
